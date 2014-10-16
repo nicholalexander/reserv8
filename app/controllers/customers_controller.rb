3423cf517221e760ce06b7e586abc33567988714
nichol alexander <nichol.alexander@gmail.com>
2014-10-16 11:22:56 -0400

    customers new, show, index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,9 +8,21 @@ class CustomersController < ApplicationController
   end
 
   def create
+    @customer = Customer.create(user_params)
+    if @customer.save
+      redirect_to customers_path
+    else
+      redirect_to root_url :notice => "already exists"
+    end
   end
 
   def show
+    @customer = Customer.find_by_id(params["id"])
+  end
+
+  private
+  def user_params
+    params.require(:customer).permit(:first_name, :email, :password, :password_confirmation)
   end
 
 
