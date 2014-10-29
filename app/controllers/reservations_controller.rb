efe89db13bb1cd2d431b3b159af1d4dcad37d196
nichol alexander <nichol.alexander@gmail.com>
2014-10-29 09:46:20 -0400

    some changes

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,13 +9,13 @@ class ReservationsController < ApplicationController
 
   def buy_reservation
     reservation = Reservation.find_by_id(params["reservation_id"])
-    paid = false
-
+    
     #charge the user money
     #TODO wrap api key into environmental variable
     Stripe.api_key = "sk_test_li0Cx9dE5T4vdnxpTdt1tb4F"
 
     #TODO convert_reservation_price / basically mult by 100
+    #TODO error handing https://stripe.com/docs/api#errors
     charge = Stripe::Charge.create(:amount => reservation.price.to_i * 100,
                           :currency => "usd",
                           :customer => current_customer.stripe_id,
