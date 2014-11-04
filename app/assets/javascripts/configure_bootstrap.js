9042c43a49d1b8bcca1eff67ccfcb764a19ac0db
nichol alexander <nichol.alexander@gmail.com>
2014-11-04 15:05:10 -0500

    new user form layout

---
+++ app/assets/javascripts/configure_bootstrap.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    
+  console.log('turn off tooltips');
+  $('[rel=tooltip]').tooltip('disable')
+  
+};
+
+
+
--- app/controllers/API/v1/welcome_controller.rb
+++ app/controllers/API/v1/welcome_controller.rb
@@ -5,7 +5,7 @@ module API
       respond_to :json
 
       def index
-        respond_with "Welcome to Reserv8.es API V1"
+        respond_with "Welcome to Reserv8.es API V1."
       end
 
     end
